59e58aaf722833a19a4fd29e147a29d43df7787d
Mattia Collalti <collalti.mattia@gmail.com>
2023-02-21 18:02:17 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { useState } from 'react';
-import { UncontrolledForm } from './UncontrolledForm';
-import { ControlledForm } from './ControlledForm';
-import { UncontrolledModal } from './UncontrolledModal';
-import { ControlledModal } from './ControlledModal';
+import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow'
 
+const Step1 = ({ goNext }) => (<>
+	<p>Step 1</p>
+	<button onClick={goNext}>Go next</button>
+</>)
+const Step2 = ({ goNext }) => (<>
+	<p>Step 2</p>
+	<button onClick={goNext}>Go next</button>
+</>)
+const Step3 = ({ goNext }) => (<>
+	<p>Step 3</p>
+	<button onClick={goNext}>Go next</button>
+</>)
 function App() {
-	const [shouldShowModal, setShouldShowModal] = useState(false);
-
 	return (
-		<>
-		<ControlledModal
-			shouldShow={shouldShowModal}
-			onRequestClose={() => setShouldShowModal(false)}
-		>
-			<h1>Hello!</h1>
-		</ControlledModal>
-		<button onClick={() => setShouldShowModal(!shouldShowModal)}>
-			{shouldShowModal ? 'Hide Modal' : 'Show Modal'}
-		</button>
-		</>
+		<UncontrolledOnboardingFlow>
+			<Step1 />
+			<Step2 />
+			<Step3 />
+		</UncontrolledOnboardingFlow>
 	);
 }
 
